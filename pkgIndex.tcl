69952aae719cd9eb1dfd76dc8ca27eefac9518c5
Danilo Chang <ray2501@gmail.com>
2023-12-16 12:12:55 +0800

    Setup version to v0.4.3

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded lmdb 0.4.2 \
-	    [list load [file join $dir libtcl9lmdb0.4.2.so] [string totitle lmdb]]
+    package ifneeded lmdb 0.4.3 \
+	    [list load [file join $dir libtcl9lmdb0.4.3.so] [string totitle lmdb]]
 } else {
-    package ifneeded lmdb 0.4.2 \
-	    [list load [file join $dir liblmdb0.4.2.so] [string totitle lmdb]]
+    package ifneeded lmdb 0.4.3 \
+	    [list load [file join $dir liblmdb0.4.3.so] [string totitle lmdb]]
 }
